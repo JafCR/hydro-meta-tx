16d8414ab59f33cb032597a41096feb068e1839e
robert.magier@gmail.com <robert.magier@gmail.com>
2020-03-04 01:40:00 -0500

    New subdir

---
+++ packages/SmartContracts/README.md
@@ -0,0 +1,2 @@
+Based on https://github.com/Lamarkaz/Metacash-contracts
+
--- packages/SmartContracts/truffle/contracts/TestERC20.sol
+++ packages/SmartContracts/truffle/contracts/TestERC20.sol
@@ -2,15 +2,17 @@ pragma solidity 0.5.16;
 
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 
 
-contract TestERC20 is ERC20,ERC20Detailed
+contract TestERC20 is ERC20,ERC20Detailed,ERC20Mintable
 {
 
     constructor() public 
     ERC20Detailed("Name", "Symbol",8) {
 
     }
+
 
 
 
